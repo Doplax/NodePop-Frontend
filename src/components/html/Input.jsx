274d537bd68cecef92fef3520e605368dc1ff2a2
Doplax <doplax@gmail.com>
2023-12-01 21:17:16 +0100

    Creación de estructura

---
+++ src/components/html/Input.jsx
@@ -0,0 +1,18 @@
+import styled  from 'styled-components'
+
+
+export const Input = styled.input`
+border-radius: 8px;
+    box-shadow: inset 0px 0px 0px 1px #5c7a89;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    gap: 5px;
+    margin: 5px;
+    padding: 20px 16px 20px 16px;
+    position: relative;
+    min-height: 24px;
+    transition: min-height 0.25s, padding 0.25s;
+    box-sizing: content-box;
+    text-align: start;
+`
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,52 @@
+import { BackArrow } from '@components/svg/BackArrow'
+import { Cross } from '@components/svg/Cross'
+import { Input } from '@components/html/Input.jsx'
+import { ChangeEvent, useEffect, useState } from 'react'
+
+export function LoginPage() {
+
+    const [name, setName] = useState('')
+
+    const handleClick = (event: ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value)
+    }
+
+
+    console.log(name);
+    return (
+        <div className='w-full h-full flex justify-center align-middle'>
+            <form className='bg-slate-100 w-[400px] p-8  rounded-lg ' >
+                <div className='flex justify-between  '>
+                    <BackArrow></BackArrow>
+                    <Cross></Cross>
+                </div>
+
+                <div className='my-3'>
+                    <h1 className='text-2xl font-bold '>Únete a Wallapop</h1>
+                </div>
+
+                <div className='flex flex-col'>
+                    <input onChange={handleClick} placeholder='Nombre y apellidos' type="text" />
+                    <Input placeholder='Direccion de email' type="text" />
+                    <Input placeholder='Contraseña'  type="text" />
+                    <Input placeholder='Nombre'  type="text" />
+                    <span>*Al menos 8 caracteres</span>
+                </div>
+
+                <div className='m-5 pt-10'>
+                    <div className='flex'>
+                        <input type="checkbox" />
+                        <span>Quiero recibir comunicaciones sobre promociones y novedades de Wallapop</span>                </div>
+                </div>
+                <div className='flex'>
+                    <input type="checkbox" />
+                    <span slot="label">He leído y acepto las <a href="https://about.wallapop.com/condiciones-de-uso/" target="_blank">Condiciones de uso</a> y la <a href="https://about.wallapop.com/politica-privacidad/" target="_blank">Política de privacidad</a> de Wallapop.</span>
+                </div>
+
+                <div className='w-full my-14 flex items-end justify-center'>
+                    <button className='' type='submit'>Crear una cuenta</button>
+                </div>
+            </form>
+        </div>
+    )
+}
