42f7a986fc6b1f69831d97ef5c01d071ee82b6dc
Doplax <doplax@gmail.com>
2023-12-12 00:25:34 +0100

    ci: Añadido estado al boton de filtros para saber cual está activado

--- src/Filters/LabelsBar.tsx
+++ src/Filters/LabelsBar.tsx
@@ -1,20 +1,36 @@
 import { Button } from "@components/styledComponents/Button";
 import { useFilterHandlers } from '../Filters/FiltersContext'
+import { useState } from 'react';
 
 export function LabelsBar() {
-  const { onSelectedTagChange }   = useFilterHandlers()
+  const { onSelectedTagChange } = useFilterHandlers();
+  const [selectedTag, setSelectedTag] = useState('');
 
   const handleClick = (event) => {
-    onSelectedTagChange(event.target.name)
-  }
+    const tagName = event.target.name;
+    setSelectedTag(tagName);
+    onSelectedTagChange(tagName);
+  };
+
+  const renderButton = (name, label) => {
+    return (
+      <Button 
+        $variant={selectedTag === name ? "fullFill" : "default"} 
+        name={name} 
+        onClick={handleClick}
+      >
+        {label}
+      </Button>
+    );
+  };
 
   return (
     <div className="flex flex-wrap justify-around m-1">
-        <Button $variant="danger" name="" onClick={handleClick}>Borrar</Button>
-        <Button $variant="default" name="lifestyle" onClick={handleClick}> Lifestyle</Button>
-        <Button $variant="default" name="mobile" onClick={handleClick}> Mobile</Button>
-        <Button $variant="default" name="motor" onClick={handleClick}> Motor</Button>
-        <Button $variant="default" name="work" onClick={handleClick}> Work</Button>
+        {renderButton("", "Sin Filtros")}
+        {renderButton("lifestyle", "Lifestyle")}
+        {renderButton("mobile", "Mobile")}
+        {renderButton("motor", "Motor")}
+        {renderButton("work", "Work")}
     </div>
-  )
+  );
 }
